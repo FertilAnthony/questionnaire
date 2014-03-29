763fed863495fe88ec7dd8b22e7844f743ed12f3
Anthony FERTIL <anthony.fertil@niji.fr>
2014-03-29 18:31:05 +0100

    Ajout lib js utiles (backbone, require ...) ainsi que bootstrap

---
+++ web/js/main.js
@@ -0,0 +1,36 @@
+require.config({
+    paths: {
+        'jquery': 'lib/jquery-2.1.0.min',
+        'underscore': 'lib/underscore-min',
+        'backbone': 'lib/backbone-min',
+        'backbone-paginator': 'lib/backbone.paginator',
+        'bootstrap': 'lib/bootstrap.min',
+        'bootstrap-datepicker': 'lib/bootstrap-datepicker',
+        // Models
+        // Collections
+        // Views
+
+    },
+    shim: {
+        'underscore': {
+            deps: [],
+            exports: '_'
+        },
+        'backbone': {
+            deps: ['jquery', 'underscore'],
+            exports: 'Backbone'
+        },
+        'backbone-paginator': {
+            deps: ['underscore', 'jquery', 'backbone'],
+            exports: 'Backbone.Paginator'
+        },
+        'bootstrap': {
+            deps: ['jquery'],
+            exports: 'Bootstrap'
+        },
+        'bootstrap-datepicker': {
+            deps: ['jquery', 'bootstrap'],
+            exports: 'Datepicker'
+        }
+    }
+});
