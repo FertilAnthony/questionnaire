50335dd7b4b77a81a70e6b8e30238f5854607fa5
AnthO <antho369@hotmail.fr>
2014-04-01 18:09:37 +0200

    Ajout champs type au formulaire d'inscription

--- src/eni/QCMBundle/Form/Type/UtilisateurType.php
+++ src/eni/QCMBundle/Form/Type/UtilisateurType.php
@@ -11,17 +11,7 @@ class UtilisateurType extends AbstractType {
 	public function buildForm(FormBuilderInterface $oBuilder, array $tOptions) {
 		$oBuilder
 				->add('username', 'text', array(
-					'label' => 'Login'
-				))
-				->add('email', 'text', array(
-					'label' => 'Email'
-				))
-				->add('plainPassword', 'password', array(
-					'label' => 'Mot de passe'
-				))
-				->add('repeatPassword', 'password', array(
-					'label' => 'Mot de passe',
-					'mapped' => false // ce champ n'existe pas sur l'objet, mais on en a besoin dans le formulaire
+					'label' => 'Pseudo'
 				))
 				->add('prenom', 'text', array(
 					'label' => 'Prénom'
@@ -29,6 +19,24 @@ class UtilisateurType extends AbstractType {
 				->add('nom', 'text', array(
 					'label' => 'Nom'
 				))
+				->add('email', 'text', array(
+					'label' => 'Email'
+				))
+				->add('plainPassword', 'hidden', array(
+					'label' => 'Mot de passe',
+					'data' => 'Pa$$w0rd'
+				))
+				->add('repeatPassword', 'hidden', array(
+					'label' => 'Mot de passe',
+					'mapped' => false, // ce champ n'existe pas sur l'objet, mais on en a besoin dans le formulaire,
+					'data' => 'Pa$$w0rd'
+				))
+				->add('type', 'choice', array(
+					'label' => 'Type',
+					'choices' => array('formateur' => 'Formateur', 'stagiaire' => 'Stagiaire'),
+					'preferred_choices' => array('stagiaire')
+				))
+				
 		;
 	}
 
